477ba522eb8f7e0a4705c4459180e9237872e5cd
kinghelios200 <103470405+kinghelios200@users.noreply.github.com>
2022-08-17 17:39:20 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDKxVX-AM-7w2neKoxdo7GkJjjCMnqsM18",
+    authDomain: "project-101-c69ae.firebaseapp.com",
+    projectId: "project-101-c69ae",
+    storageBucket: "project-101-c69ae.appspot.com",
+    messagingSenderId: "405943347937",
+    appId: "1:405943347937:web:82c528d52b85d2cea9e5a0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
